31c651669877f7266e450c4390b84f4b142095d7
emachierici <emachierici000@gmail.com>
2023-05-11 18:45:55 +0200

    volume controller update

--- volume_controller.vhd
+++ volume_controller.vhd
@@ -15,14 +15,14 @@ entity volume_controller is
 
 		-- Master
 		m_axis_tvalid	: out STD_LOGIC;
-		m_axis_tdata	: out STD_LOGIC_VECTOR(7 downto 0);
+		m_axis_tdata	: out STD_LOGIC_VECTOR(23 downto 0);
 		m_axis_tready	: in STD_LOGIC;
         m_axis_tlast    : out STD_LOGIC;
 
 		-- Slave
 
 		s_axis_tvalid	: in STD_LOGIC;
-		s_axis_tdata	: in STD_LOGIC_VECTOR(7 downto 0);
+		s_axis_tdata	: in STD_LOGIC_VECTOR(23 downto 0);
         s_axis_tready	: out STD_LOGIC;
         s_axis_tlast    : in STD_LOGIC;
 
@@ -33,19 +33,21 @@ end volume_controller;
 
 architecture Behavioral of volume_controller is
 
-    constant SPAN : integer := 2 ** N_VALUE;
-    constant SPAN_HALF : integer := SPAN / 2;
+    constant SPAN             : integer     := 2 ** N_VALUE;
+    constant SPAN_HALF        : integer     := SPAN / 2;
     
-    constant NUMBER_OF_GROUPS : integer  := 2 ** 10 / SPAN;   
+    constant NUMBER_OF_GROUPS : integer     := 2 ** 10 / SPAN;   
     
     
 
-    signal volume_integer : integer := to_integer(unsigned(volume));
-    signal volume_temp : integer := to_integer(unsigned(volume));
-    signal LorR : std_logic := '0'; --0 is division, 1 is multiplication
-    signal slider : integer := 0;
+    signal volume_integer : integer   := to_integer(unsigned(volume));
+    signal volume_temp    : integer   := to_integer(unsigned(volume));
+    signal LorR           : std_logic := '0';                           -- 0 is division, 1 is multiplication
+    signal slider         : integer   := 0;
 
-    signal finished_flag : std_logic := '0';
+    signal finished_flag  : std_logic := '0';   -- maybe we don't need this flag since we have slider and volume_temp that can check the state : if finished or not
+    signal counter        : integer;
+    signal output_temp    : std_logic_vector := s_axis_tdata;
     
 	
 begin
@@ -55,24 +57,64 @@ begin
 	range_finder : process(aclk)
 		begin
 			volume_temp <= volume_integer - 512;
-            slider <= 0;
+            slider  <= 0;
+            counter <= 0;
 
             if volume_temp > 0 then
                 LorR <= '1';
             else LorR <= '0';
             end if;
 
-            while finished_flag = '0' loop
-                slider <= slider + SPAN_HALF;
-                
-                if volume_temp < slider then
+            if LorR = '1' then
+                while volume_temp > slider loop
+                    if slider = 0 then
+                        slider <= slider + SPAN_HALF;
+                    else 
+                        slider <= slider + SPAN;
+                        counter <= counter + 1;
+                    end if;
+                    
+    --                if volume_temp < slider then
+    --
+    --                    finished_flag <= '1';
+    --                end if; 
+                end loop;
+                if slider > volume_temp then
 
-                    finished_flag <= '1';
-                end if; 
+                    while counter /= 0 loop  -- <shift_left>            NON HO CONTROLLATO IL CLIPPING
+
+                        output_temp(23 downto 0) <= output_temp(22 downto 0) & '0';
+                        counter <= counter - 1;
+
+                    end loop;
+
+                end if;
+            end if;
+
+            if LorR = '0' then
+                while volume_temp < slider loop
+                    if slider = 0 then
+                        slider <= slider - SPAN_HALF;
+                    else 
+                        slider <= slider - SPAN;
+                        counter <= counter + 1;
+                    end if;
+
+                end loop;
+                if slider <  volume_temp then
+                    while counter /= 0 loop  -- <shift_right>
+
+                        output_temp(23 downto 0) <= '0' & output_temp(23 downto 1);
+                        counter <= counter - 1;
+
+                    end loop;
+
+                end if;
+            end if;
 
 
-            end loop;
-            
+
+
 
 
 
