e539fe0a93cfa7f8b3b54eca1b473d2d145783a6
mario-calio <56538989+mario-calio@users.noreply.github.com>
2023-05-12 18:24:16 +0200

    Volume finished

--- volume_controller.vhd
+++ volume_controller.vhd
@@ -6,7 +6,7 @@ library IEEE;
 entity volume_controller is
     
 	generic (
-		N_VALUE		: integer := 6 
+		N_VALUE		    : integer := 6 
         );
 	Port ( 
         -- 
@@ -40,89 +40,118 @@ architecture Behavioral of volume_controller is
     
     
 
-    signal volume_integer : integer   := to_integer(unsigned(volume));
-    signal volume_temp    : integer   := to_integer(unsigned(volume));
-    signal LorR           : std_logic := '0';                           -- 0 is division, 1 is multiplication
-    signal slider         : integer   := 0;
+    signal volume_integer     : integer   := to_integer(unsigned(volume));
+    signal volume_temp        : integer   := to_integer(unsigned(volume));
+    signal LorR               : std_logic := '0';                             -- 0 is division, 1 is multiplication
+    signal m_axis_tlast_temp  : std_logic := '0';
+    signal s_axis_tready_int  : std_logic := '0';
+    signal slider             : integer   :=  0;
 
-    signal finished_flag  : std_logic := '0';   -- maybe we don't need this flag since we have slider and volume_temp that can check the state : if finished or not
-    signal counter        : integer;
-    signal output_temp    : std_logic_vector := s_axis_tdata;
+    -- signal finished_flag      : std_logic := '0';   -- maybe we don't need this flag since we have slider and volume_temp that can check the state : if finished or not
+    signal counter            : integer;
+    signal output_temp        : std_logic_vector(23 downto 0) := s_axis_tdata;
     
 	
 begin
 
+    s_axis_tready <= s_axis_tready_int;
 	volume_integer <= to_integer(unsigned(volume));
 
 	range_finder : process(aclk)
-		begin
-			volume_temp <= volume_integer - 512;
-            slider  <= 0;
+    begin
+        if aresetn = '0' then
+            slider <= 0;
             counter <= 0;
+            output_temp <= (others => '0');
+        elsif rising_edge(aclk) then
 
-            if volume_temp > 0 then
-                LorR <= '1';
-            else LorR <= '0';
-            end if;
+            m_axis_tvalid <= '0';            
+            s_axis_tready_int <= '1'; -- check if you skip a cycle untill s axis t ready isn't 1
 
-            if LorR = '1' then
-                while volume_temp > slider loop
-                    if slider = 0 then
-                        slider <= slider + SPAN_HALF;
-                    else 
-                        slider <= slider + SPAN;
-                        counter <= counter + 1;
-                    end if;
-                    
-    --                if volume_temp < slider then
-    --
-    --                    finished_flag <= '1';
-    --                end if; 
-                end loop;
-                if slider > volume_temp then
+            if s_axis_tready_int = '1' and s_axis_tvalid = '1' then
 
-                    while counter /= 0 loop  -- <shift_left>            NON HO CONTROLLATO IL CLIPPING
+                m_axis_tlast_temp <= s_axis_tlast;
+                s_axis_tready_int <= '0'; --we're always ready actually
+                output_temp <= s_axis_tdata;
 
-                        output_temp(23 downto 0) <= output_temp(22 downto 0) & '0';
-                        counter <= counter - 1;
 
+                volume_temp <= volume_integer - 512;
+                slider  <= 0;
+                counter <= 0;
+
+                if volume_temp > 0 then
+                    LorR <= '1';
+                else LorR <= '0';
+                end if;
+
+                if LorR = '1' then
+                    while volume_temp > slider loop
+                        if slider = 0 then
+                            slider <= slider + SPAN_HALF;
+                        else 
+                            slider <= slider + SPAN;
+                            counter <= counter + 1; 
+                        end if;
+                        
                     end loop;
 
-                end if;
-            end if;
+                    if volume_temp <= slider then
 
-            if LorR = '0' then
-                while volume_temp < slider loop
-                    if slider = 0 then
-                        slider <= slider - SPAN_HALF;
-                    else 
-                        slider <= slider - SPAN;
-                        counter <= counter + 1;
+                        
+
+                        while counter /= 0 loop  -- <shift_left>
+                            
+                            if to_signed(volume_temp, 24)(23) = '1' then   -- CHECK CLIPPING
+                                counter <= 0;
+                                output_temp <= ((others => '1') );
+
+                            else
+                                
+                                output_temp(23 downto 0) <= output_temp(22 downto 0) & '0';
+                                counter <= counter - 1;
+
+                            end if;
+
+                        end loop;
+
+                            
+                        m_axis_tdata <= output_temp;
+                        m_axis_tvalid <= '1';
+                        m_axis_tlast <= m_axis_tlast_temp;
+
                     end if;
+                end if;
 
-                end loop;
-                if slider <  volume_temp then
-                    while counter /= 0 loop  -- <shift_right>
-
-                        output_temp(23 downto 0) <= '0' & output_temp(23 downto 1);
-                        counter <= counter - 1;
+                if LorR = '0' then
+                    while volume_temp < slider loop
+                        if slider = 0 then
+                            slider <= slider - SPAN_HALF;
+                        else 
+                            slider <= slider - SPAN;
+                            counter <= counter + 1;
+                        end if;
 
                     end loop;
+                    if slider <=  volume_temp then
 
+                        while counter /= 0 loop  -- <shift_right>
+
+                            output_temp(23 downto 0) <= '0' & output_temp(23 downto 1);
+                            counter <= counter - 1;
+
+                        end loop;                
+
+                        m_axis_tdata <= output_temp;
+                        m_axis_tvalid <= '1';
+                        
+                    end if;
                 end if;
+
             end if;
 
-
-
-
-
-
-
+        end if;
 
 	end process;
+
 	
-	jstk_X_Y_Button_receive : process(aclk)
-		begin
-		
-	end process;
 end architecture;
