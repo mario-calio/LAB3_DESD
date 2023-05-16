28ca42240cdc48fe6e444cada1c67f6d549aebe5
valeriodimalta <10699498@polimi.it>
2023-05-16 16:51:35 +0200

    Update dual_moving_average.vhd

--- dual_moving_average.vhd
+++ dual_moving_average.vhd
@@ -44,6 +44,7 @@ architecture Behavioral of dual_moving_average is
     signal average_dx : std_logic_vector(29 DOWNTO 0) := (Others => '0');
     signal average_sx : std_logic_vector(29 DOWNTO 0) := (Others => '0');
     
+    signal s_axis_tready_int : std_logic := '1';
     signal m_axis_tdata_int : std_logic_vector (23 DOWNTO 0) := (Others => '0');
     signal m_axis_tvalid_int : std_logic := '0';
     signal m_axis_tlast_temp  : std_logic := '0';
@@ -53,6 +54,7 @@ architecture Behavioral of dual_moving_average is
 
 begin
     
+    s_axis_tready <= s_axis_tready_int;
     m_axis_tvalid <= m_axis_tvalid_int;
 
     process (aclk,aresetn)
@@ -64,17 +66,21 @@ begin
             counter_dx <= 0;
             counter_sx <= 0;
             s_axis_tready <= '1';
-            m_axis_tvalid_int <= '0';
+            m_axis_tvalid <= '0';
             new_data <= '0';
 
         elsif rising_edge(aclk) then
-             
+
+            if filter_enable = '1' then
+            is_filter <= not is_filter;
+            end if;
+
             --slave--------------      
     --If data is valid from the master
             if s_axis_tvalid = '1' then
     --I check if the filter is enabled 
                 new_data <= '1';
-                if filter_enable = '1' then
+                if is_filter = '1' then
     --I check what channel is the data for
                     if s_axis_tlast = '1' then
                         if counter_dx = 32 then
@@ -83,13 +89,13 @@ begin
                             sum_dx <= std_logic_vector(signed(sum_dx) + signed(mem_dx(31)));
                             average_dx <= std_logic_vector(signed(sum_dx)/32);
                             m_axis_tdata_int <= average_dx(23 DOWNTO 0);
-                            m_axis_tlast <= m_axis_tlast_temp;
+                            m_axis_tlast_temp <= s_axis_tlast;
                         else
                             mem_dx(counter_dx) <= s_axis_tdata;
                             sum_dx <= std_logic_vector(signed(sum_dx) + signed(s_axis_tdata));
                             counter_dx  <= counter_dx + 1;
                             m_axis_tdata_int <= s_axis_tdata;
-                            m_axis_tlast <= m_axis_tlast_temp;
+                            m_axis_tlast_temp <= s_axis_tlast;
                         end if;
                             
                     else
@@ -99,27 +105,30 @@ begin
                             sum_sx <= std_logic_vector(signed(sum_sx) + signed(mem_sx(31)));
                             average_sx <= std_logic_vector(signed(sum_sx)/32);    
                             m_axis_tdata_int <= average_sx(23 DOWNTO 0);  
-                            m_axis_tlast <= m_axis_tlast_temp;      
+                            m_axis_tlast_temp <= s_axis_tlast;      
                         else 
                             mem_dx(counter_sx) <= s_axis_tdata;
                             sum_sx <= std_logic_vector(signed(sum_sx) + signed(s_axis_tdata));
                             counter_sx  <= counter_sx + 1;
                             m_axis_tdata_int <= s_axis_tdata;
-                            m_axis_tlast <= m_axis_tlast_temp;
+                            m_axis_tlast_temp <= s_axis_tlast;
                         end if;
                     end if;
                 else
                     m_axis_tdata_int <= s_axis_tdata;
-                    m_axis_tlast <= m_axis_tlast_temp;
+                    m_axis_tlast_temp <= s_axis_tlast;
                 end if;
             end if;
             --------------
             --- master-----
 
-            --a cosa serve checkare is filter?
-            if filter_enable = '1' then
-                is_filter <= not is_filter;
-            end if;
+            --a cosa serve checkare is filter? Risposta: va messo, come per 'mute', la variabile 'is_filter' 
+            --per fare la verifica del filtraggio e quindi abbiamo bisogno dell'if qui sotto per far cambiare is_filter quando viene
+            --premuto il tasto del joystick
+
+            -- if filter_enable = '1' then
+            --     is_filter <= not is_filter;
+            -- end if;
             
             if new_data <= '1' and m_axis_tvalid_int = '0' then
                 
